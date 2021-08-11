41ce18984a11fc13d85a3639bb830f2973dc85c2
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-11 21:34:52 +0100

    Created task resucers: createTask and getTasks

---
+++ redux/reducers/taskReducers.js
@@ -0,0 +1,35 @@
+import { ActionSheetIOS } from "react-native";
+import {
+  CREATE_TASK_FAIL,
+  CREATE_TASK_REQUEST,
+  CREATE_TASK_SUCCESS,
+  GET_TASKS_FAIL,
+  GET_TASKS_REQUEST,
+  GET_TASKS_SUCCESS,
+} from "../constants/taskConstants";
+
+export const getTasksReducer = (state = [], action) => {
+  switch (action.type) {
+    case GET_TASKS_REQUEST:
+      return { loading: true };
+    case GET_TASKS_SUCCESS:
+      return { loading: false, projects: action.payload };
+    case GET_TASKS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const createTaskReducer = (state = {}, action) => {
+  switch (action.type) {
+    case CREATE_TASK_REQUEST:
+      return { loading: true };
+    case CREATE_TASK_SUCCESS:
+      return { loading: false, task: action.payload };
+    case CREATE_TASK_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};

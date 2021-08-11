0d432dca663f5b2228900e54a1102f8575d920f9
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-11 21:29:32 +0100

    Created getTasks and createTask actions in redux

---
+++ redux/actions/taskActions.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  CREATE_TASK_FAIL,
+  CREATE_TASK_REQUEST,
+  CREATE_TASK_SUCCESS,
+  GET_TASKS_FAIL,
+  GET_TASKS_REQUEST,
+  GET_TASKS_SUCCESS,
+  GET_TASK_REQUEST,
+  GET_TASK_SUCCESS,
+} from "../constants/taskConstants";
+
+export const getTasks = (projectSlug) => async (dispatch) => {
+  try {
+    dispatch({ type: GET_TASKS_REQUEST });
+    const token = await AsyncStorage.getItem("token");
+    const { data } = await axios.get(
+      `https://project-management-2.herokuapp.com/api/${projectSlug}/tasks`,
+      { headers: { token } }
+    );
+    dispatch({ type: GET_TASKS_SUCCESS, payload: data.tasks });
+  } catch (error) {
+    dispatch({
+      type: GET_TASKS_FAIL,
+      payload: error.response.data.message
+        ? error.response.data.message
+        : error.message,
+    });
+  }
+};
+
+export const createTask = (task, projectSlug) => async (dispatch) => {
+  try {
+    dispatch({ type: CREATE_TASK_REQUEST });
+    const token = await AsyncStorage.getItem("token");
+    const result = await axios.post(
+      `https://project-management-2.herokuapp.com/api/${projectSlug}/tasks`,
+      task,
+      { headers: { token } }
+    );
+    dispatch({ type: CREATE_TASK_SUCCESS, payload: result.data.task });
+    const { data } = await axios.get(
+      `https://project-management-2.herokuapp.com/api/${projectSlug}/tasks`,
+      { headers: { token } }
+    );
+    dispatch({ type: GET_TASKS_SUCCESS, payload: data.tasks });
+  } catch (error) {
+    dispatch({
+      type: CREATE_TASK_FAIL,
+      payload: error.response.data.message
+        ? error.response.data.message
+        : error.message,
+    });
+  }
+};

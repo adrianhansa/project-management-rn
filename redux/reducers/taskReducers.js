4a8126b7109222235a3ea623b81b44375a8485ba
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-12 23:14:42 +0100

    Created updateTask, getTask and deleteTask reducers

--- redux/reducers/taskReducers.js
+++ redux/reducers/taskReducers.js
@@ -3,9 +3,18 @@ import {
   CREATE_TASK_FAIL,
   CREATE_TASK_REQUEST,
   CREATE_TASK_SUCCESS,
+  DELETE_TASK_FAIL,
+  DELETE_TASK_REQUEST,
+  DELETE_TASK_SUCCESS,
   GET_TASKS_FAIL,
   GET_TASKS_REQUEST,
   GET_TASKS_SUCCESS,
+  GET_TASK_FAIL,
+  GET_TASK_REQUEST,
+  GET_TASK_SUCCESS,
+  UPDATE_TASK_FAIL,
+  UPDATE_TASK_REQUEST,
+  UPDATE_TASK_SUCCESS,
 } from "../constants/taskConstants";
 
 export const getTasksReducer = (state = [], action) => {
@@ -28,6 +37,45 @@ export const createTaskReducer = (state = {}, action) => {
     case CREATE_TASK_SUCCESS:
       return { loading: false, task: action.payload };
     case CREATE_TASK_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const getTaskReducer = (state = {}, action) => {
+  switch (action.type) {
+    case GET_TASK_REQUEST:
+      return { loading: true };
+    case GET_TASK_SUCCESS:
+      return { loading: false, task: action.payload };
+    case GET_TASK_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const updateTaskReducer = (state = {}, action) => {
+  switch (action.type) {
+    case UPDATE_TASK_REQUEST:
+      return { loading: true };
+    case UPDATE_TASK_SUCCESS:
+      return { loading: false, task: action.payload };
+    case UPDATE_TASK_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const deleteTaskReducer = (state = {}, action) => {
+  switch (action.type) {
+    case DELETE_TASK_REQUEST:
+      return { loading: true };
+    case DELETE_TASK_SUCCESS:
+      return { loading: false, task: action.payload };
+    case DELETE_TASK_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
